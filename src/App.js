3e82049bb8bcb74822f59e1b720b732c4142b955
fberroeta <fsberroeta@gmail.com>
2021-12-01 21:56:20 -0300

    com entre clase hijapadre

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
 // import ExpenseItem from "./components/ExpenseItem";
-import Expenses from "./components/Expenses/Expenses";
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/Expenses/NewExpense';
 
 function App() {
   const expenses = [
     {
-      id: "e1",
-      title: "Comida",
+      id: 'e1',
+      title: 'Comida',
       amount: 80000,
       date: new Date(2020, 7, 14),
     },
     {
-      id: "e2",
-      title: "Nueva TV",
+      id: 'e2',
+      title: 'Nueva TV',
       amount: 350000,
       date: new Date(2021, 2, 12),
     },
     {
-      id: "e3",
-      title: "Seguro Auto",
+      id: 'e3',
+      title: 'Seguro Auto',
       amount: 50000,
       date: new Date(2021, 2, 28),
     },
     {
-      id: "e4",
-      title: "Cuenta Internet",
+      id: 'e4',
+      title: 'Cuenta Internet',
       amount: 40000,
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2> Let 's get started!</h2> <Expenses expenses={expenses} />{" "}
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
